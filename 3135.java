33fd1976444ab219b227faaba908b0a838fed985
Huiwon Jo <50469773+huihuiwon@users.noreply.github.com>
2022-08-11 21:39:59 +0900

    Create 3135.java

---
+++ 3135.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt(); int b=sc.nextInt();
+		int org_a=a;
+		int n=sc.nextInt();
+		int star[]=new int[n];
+		for(int i=0; i<n; i++) {
+			star[i]= sc.nextInt();
+			if(b==star[i]) {
+				System.out.println(1);
+				return;
+			}
+		}
+		
+		int result = 0;
+		
+		while(a != b) {  // 무조건 즐겨찾기를 한 번은 사용했을 때 최소 경우
+			int tmpi=1001; int tmp=1001;
+			for(int s : star) {
+				if(tmp > Math.abs(b-s) && tmpi!=s) {
+					tmp = Math.abs(b-s);
+					tmpi = s;
+				}
+			}
+
+			result += Math.min(Math.abs(a-tmpi), 1);
+			if(a==tmpi) {
+				result += Math.abs(b-a);
+				a=b;
+				break;
+			}
+			a=tmpi;
+		}
+		
+		System.out.println(Math.min(result, Math.abs(b-org_a)));  // 즐겨찾기를 사용하지 않았을 때와 비교
+	}
+	
+}
